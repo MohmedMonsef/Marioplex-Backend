b819d39c990a9871d97417f2eedd142077f98016
dina alaa <dinaalaaahmed@gmail.com>
2020-03-06 19:23:28 +0200

    dataBase collections

---
+++ models/artist.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const Schema= mongoose.Schema;
+const User= require('./user');
+// create user schema
+
+
+module.exports = Artist;
---
+++ models/image.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const Schema= mongoose.Schema;
+
+// create image schema
+
+
+module.exports = Image;

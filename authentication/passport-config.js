bb21016c58c85320b28593e75d2a13330c32b2b0
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-03-11 18:52:30 +0200

    removed cookies and user serialization
    TO DO
    IMPLEMENT JWT

--- authentication/passport-config.js
+++ authentication/passport-config.js
@@ -4,14 +4,5 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 
 // TO DO
-// DONT SAVE COOKIES
+
 // IMPLEMENT JWT
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-  
-passport.deserializeUser(function(id, done) {
-   
-      done(null, id);
-   
-});

09c43ca56b4b3e0415e89ea50a08e9f8becaf9f3
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-03-13 22:44:33 +0200

    atlas connection

--- DBconnection/connection.js
+++ DBconnection/connection.js
@@ -6,7 +6,7 @@ mongoose.Promise=global.Promise;
 
 module.exports= function(app){
 
-    mongoose.connect('mongodb+srv://Spotify:spotifyapp@spotifycluster-i2m7n.mongodb.net/Spotify?retryWrites=true&w=majority' ,{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+    mongoose.connect('mongodb+srv://nada:nada@spotifycluster-i2m7n.mongodb.net/Spotify?retryWrites=true&w=majority' ,{  useNewUrlParser: true, useCreateIndex: true });
     mongoose.connection.once('open',()=>{
     console.log("connection is made");
     }).on('error',function(error){

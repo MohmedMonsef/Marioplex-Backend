b427d5689c90e35d96d56dc68ce5e0bbd1c39dfa
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-03-13 20:37:40 +0200

    finish facebook check auth

--- authentication/facebook-authentication.js
+++ authentication/facebook-authentication.js
@@ -3,7 +3,11 @@ const jwtSeret = require('../config/jwt-key');
 const jwt =require('jsonwebtoken');
 const FACEBOOK_APP_ID = require('./keys').facebook.FACEBOOK_APP_ID;
 const FACEBOOK_APP_SECRET = require('./keys').facebook.FACEBOOK_APP_SECRET;
+const  {user:userDocument,artist:artistDocument,album:albumDocument,track:trackDocument,playlist:playlistDocument,category:categoryDocument} = require('../models/db');
 
+
+// initialize db 
+const connection=require('../DBconnection/connection');
 // get jwt middleware
 const JWT = require('../config/passport-jwt');
 
@@ -11,14 +15,36 @@ module.exports = (passport) => {
         passport.use(new FacebookStrategy({
             clientID: FACEBOOK_APP_ID,
             clientSecret: FACEBOOK_APP_SECRET,
-            callbackURL: "/auth/facebook/callback"
+            callbackURL: "/auth/facebook/callback",
+            profileFields: ['id', 'emails', 'name'] 
         },
-        function(accessToken, refreshToken, profile, done) {
+        async function(accessToken, refreshToken, profile, done) {
               // TO DO 
               // CHECK IF USER IN DB IF TRUE THEN LOG HIM IN ELSE CREATE A NEW ACCOUNT FOR USER
                if(profile){
-                    
-                   return done(null,profile);
+                   //console.log(profile);
+                    const user = await userDocument.findOne({ email:profile.emails[0].value },(err,user)=>{
+                        if(err) return 0;
+                        return user;
+                    });
+                    if(user){
+                        // user in db
+                       
+                        return done(null,user);
+                    }else{
+                        // create user
+                        const newUser = await new userDocument({
+                            email:profile.emails[0].value,
+                            displayName:profile.username,
+                            gender:profile.gender,
+
+
+
+                        }).save();
+                       
+                        return done(null,newUser);
+                    }
+                   
                }
                return done(true,null);
         }
--- authentication/facebook-routes.js
+++ authentication/facebook-routes.js
@@ -1,17 +1,53 @@
 const router = require('express').Router();
 const passport = require('passport');
-const token = require('../config/generate-token');
+
+const jwtSeret = require('../config/jwtconfig');
+const jwt =require('jsonwebtoken');
 // initialize passport
 require('./passport-config')(passport);
+const  {user:userDocument,artist:artistDocument,album:albumDocument,track:trackDocument,playlist:playlistDocument,category:categoryDocument} = require('../models/db');
+
+
+// initialize db 
+const connection=require('../DBconnection/connection');
 // initialize the strategy
 require('./facebook-authentication')(passport);
 
+// serialize user
+passport.serializeUser(function(user, done) {
+  
+    done(null, user.id);
+});
+  
+passport.deserializeUser(async function(id, done) {
+    await userDocument.findById(id, function(err, user) {
+      
+      done(err, user);
+    });
+});
 
+function checkAuthentication(req,res,next){
+    if(req.isAuthenticated()){
+        //req.isAuthenticated() will return true if user is logged in
+        next();
+    } else{
+        res.redirect("/login");
+    }
+}
+router.get('/facebook', passport.authenticate('facebook',{ scope:[ 'email'] }));
 
+router.get('/facebook/callback',passport.authenticate('facebook', {  successRedirect: '/auth/facebookJWT',failureRedirect: '/login' }));
 
-router.get('/facebook', passport.authenticate('facebook'));
-
-router.get('/facebook/callback',passport.authenticate('facebook', { session: false, successRedirect: '/',failureRedirect: '/login' }));
-
+router.get('/facebookJWT',checkAuthentication,(req,res)=>{
+    const id = req.session.passport.user;
+    //res.send('hh')
+   var token = jwt.sign({ _id: id}, jwtSeret.secret, {
+        expiresIn: 60 // 1 week
+     });
+     
+      // return the information including token as JSON
+      res.json({success: true, token: 'JWT ' + token});
+      res.send(token);
+});
 
 module.exports = router;

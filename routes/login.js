e3228793e305941f655ed3edacc9aadf7d87a446
ayaabohadima <ayasabohadima@gmail.com>
2020-03-28 17:41:39 +0200

    test-artist

--- routes/login.js
+++ routes/login.js
@@ -41,7 +41,7 @@ router.post("/login", (req, res) => {
             
           };
           var token = jwt.sign({ _id: user._id,product: user.product,userType:user.userType}, jwtSeret.secret, {
-            expiresIn: '24h' // 1 day
+            expiresIn: '36767467347h' // 1 day
           });
          
           // return the information including token as JSON

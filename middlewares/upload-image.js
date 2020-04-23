b813b33f0d9491978ddbf3b5d6b44a25b405c566
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-04-23 13:33:35 +0200

    added ong and imageID

--- middlewares/upload-image.js
+++ middlewares/upload-image.js
@@ -16,7 +16,8 @@ const storage = new GridFsStorage({
       //console.log(file);
       //console.log(req.user)
       // check extension of the track to be webm audio/video 
-        let acceptedExtensions = ["image/jpeg","image/bmp"]
+      //console.log(file.mimetype)
+        let acceptedExtensions = ["image/jpeg","image/bmp","image/png","image/jpg"]
       if( !acceptedExtensions.includes( file.mimetype)){throw Error("file not supported");}
 
       return new Promise(async (resolve, reject) => {

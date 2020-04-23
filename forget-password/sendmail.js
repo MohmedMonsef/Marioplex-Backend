a33a4016f7715e31cc7ff670ddc92595b210e2a0
ayaabohadima <ayasabohadima@gmail.com>
2020-04-23 04:37:15 +0200

    env-var

--- forget-password/sendmail.js
+++ forget-password/sendmail.js
@@ -1,37 +1,38 @@
 var nodemailer = require('nodemailer');
-module.exports=async function(email,message,type){
-var transporter = nodemailer.createTransport({
-  host:'smtp.gmail.com',
-  port:587,
-  secure:false,
-  auth: {
-    user: 'appspotify646@gmail.com',
-    pass: 'Helloworld55'
-  },
-  tls:{
-    rejectUnauthorized:false
-  }
-});
-var mailOptions;
-if(type=="password"){
- mailOptions = {
-  from: '"Spotify Contact" <appspotify646@gmail.com>',
-  to: email,
-  subject: 'SPOTIFY SAMA has A Message FOR YOU ^^',
-  text:'Please follow this URL http://52.205.254.29/login/reset_password?token=' + message
-  
-};
-}
-else{
-  mailOptions = {
-    from: '"Spotify Contact" <appspotify646@gmail.com>',
-    to: email,
-    subject: 'SPOTIFY SAMA has A Message FOR YOU ^^',
-    text:message
-  };
-}
+module.exports = async function(email, message, type) {
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.SPOTIFY_EMAIL ? String(process.env.SPOTIFY_EMAIL) : 'appspotify646@gmail.com',
+            pass: process.env.SPOTIFY_PASSWORD ? String(process.env.SPOTIFY_EMAIL_PASSWORD) : 'Helloworld55'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    var mailOptions;
+    if (type == "password") {
+        mailOptions = {
+            from: '"Spotify Contact" <' + process.env.SPOTIFY_EMAIL ? String(process.env.SPOTIFY_EMAIL) : 'appspotify646@gmail.com' + '>',
+            to: email,
+            subject: 'SPOTIFY SAMA has A Message FOR YOU ^^',
+            text: 'Please follow this URL http://52.205.254.29/login/reset_password?token=' + message
 
-transporter.sendMail(mailOptions, function(error, info){
- 
-});
+        };
+    } else {
+        mailOptions = {
+            from: '"Spotify Contact" <' + process.env.SPOTIFY_EMAIL ? String(process.env.SPOTIFY_EMAIL) : 'appspotify646@gmail.com' + '>',
+            to: email,
+            subject: 'SPOTIFY SAMA has A Message FOR YOU ^^',
+            text: message
+        };
+    }
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        // if(error)
+
+
+    });
 };
--- server.js
+++ server.js
@@ -1,73 +1,74 @@
-const connection=require('./db-connection/connection');
-const express = require('express');
-const app =express();
-const rateLimit = require("express-rate-limit");
-//connect to database
-connection(app);
+  require('dotenv/config'); // to use env variable 
+  const connection = require('./db-connection/connection');
+  const express = require('express');
+  const app = express();
+  const rateLimit = require("express-rate-limit");
+  //connect to database
+  connection(app);
 
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const logger = require('morgan');
-const passport = require('passport');
-const session = require('express-session');
-const browse=require('./routes/browse');
-const album=require('./routes/album');
-const Track=require('./routes/track-routes');
-const playlist=require('./routes/playlist-routes');
-const Artist=require('./routes/artist-route');
-const Library=require('./routes/library-routes');
-const userProfile=require('./routes/user-profile-routes');
-const homePage =require('./routes/home-page-routes');
-const login=require('./routes/login');
-const signup=require('./routes/signup-routes');
-const search=require('./routes/search');
-const player = require('./routes/player-routes')
-const facebook = require('./authentication/facebook-routes');
-const forgpass = require('./routes/forgpass-route');
-const images = require('./routes/image-route')
-require('./config/passport');
-const limiter = rateLimit({
-    windowMs:  10*60 * 1000, // 15 minutes
-    max: 1 // limit each IP to 100 requests per windowMs
+  const cors = require('cors');
+  const bodyparser = require('body-parser');
+  const logger = require('morgan');
+  const passport = require('passport');
+  const session = require('express-session');
+  const browse = require('./routes/browse');
+  const album = require('./routes/album');
+  const Track = require('./routes/track-routes');
+  const playlist = require('./routes/playlist-routes');
+  const Artist = require('./routes/artist-route');
+  const Library = require('./routes/library-routes');
+  const userProfile = require('./routes/user-profile-routes');
+  const homePage = require('./routes/home-page-routes');
+  const login = require('./routes/login');
+  const signup = require('./routes/signup-routes');
+  const search = require('./routes/search');
+  const player = require('./routes/player-routes')
+  const facebook = require('./authentication/facebook-routes');
+  const forgpass = require('./routes/forgpass-route');
+  const images = require('./routes/image-route')
+  require('./config/passport');
+  const limiter = rateLimit({
+      windowMs: 10 * 60 * 1000, // 15 minutes
+      max: 1 // limit each IP to 100 requests per windowMs
   });
-app.use(cors());
-app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
-app.use(logger('dev'));
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
-//app.use('/api/',limiter);
-//routes
-app.use('/api',homePage);
-app.use('/api',Library);
-app.use('/api',forgpass);
-app.use('/api',userProfile)
-app.use('/api',search);
-app.use('/api',album);
-app.use('/api',login);
-app.use('/api',signup);
-app.use('/api',Track);
-app.use('/api',player);
-app.use('/api',playlist);
-app.use('/api/auth',facebook);
-app.use('/api',browse);
-app.use('/api',Artist);
-app.use('/api',images);
+  app.use(cors());
+  app.use(bodyparser.urlencoded({ extended: false }));
+  app.use(bodyparser.json());
+  app.use(logger('dev'));
+  app.use(session({ secret: 'anything' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  //app.use('/api/',limiter);
+  //routes
+  app.use('/api', homePage);
+  app.use('/api', Library);
+  app.use('/api', forgpass);
+  app.use('/api', userProfile)
+  app.use('/api', search);
+  app.use('/api', album);
+  app.use('/api', login);
+  app.use('/api', signup);
+  app.use('/api', Track);
+  app.use('/api', player);
+  app.use('/api', playlist);
+  app.use('/api/auth', facebook);
+  app.use('/api', browse);
+  app.use('/api', Artist);
+  app.use('/api', images);
 
-  
+
   //  apply to all requests
 
-//connect to db before test run
-const API_PORT= process.env.PORT||3000;
+  //connect to db before test run
+  const API_PORT = process.env.PORT || 3000;
 
-app.use(function(error,req,res,next){
-    res.status(500);
-    res.send({error:error.message});
-    
-});
-app.listen(process.env.port||API_PORT,function(){
-    console.log('listening for a request');
-});
+  app.use(function(error, req, res, next) {
+      res.status(500);
+      res.send({ error: error.message });
 
-module.exports = app;
+  });
+  app.listen(process.env.port || API_PORT, function() {
+      console.log('listening for a request');
+  });
+
+  module.exports = app;

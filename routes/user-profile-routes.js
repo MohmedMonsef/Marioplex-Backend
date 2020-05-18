296b67194375c1cea01df4e0c464f6a0d4b1e7e0
ayaabohadima <ayasabohadima@gmail.com>
2020-05-18 10:23:34 +0200

    edit me-player

--- routes/user-profile-routes.js
+++ routes/user-profile-routes.js
@@ -17,21 +17,21 @@ const limiter = rateLimit({
 
 //GET USER'S PUBLIC PROFILE, PATH PARAMS: id
 router.get('/users/:id', checkIfAuth, limiter, async(req, res) => {
-    if(req.isAuth){
-    const user = await User.me(req.params.id, req.user._id);
-    if (user) {
-        let curUser = await User.getUserById(req.user._id);
-        let profUser = await User.getUserById(req.params.id);
-        await profileNotification.sendProfileNotification(curUser, profUser);
-        res.send(user);
+    if (req.isAuth) {
+        const user = await User.me(req.params.id, req.user._id);
+        if (user) {
+            let curUser = await User.getUserById(req.user._id);
+            let profUser = await User.getUserById(req.params.id);
+            await profileNotification.sendProfileNotification(curUser, profUser);
+            res.send(user);
+        } else {
+            res.sendStatus(404)
+        }
     } else {
-        res.sendStatus(404)
+        const user = await User.getUnAuthUser(req.params.id);
+        if (!user) res.sendStatus(404);
+        else res.status(200).json(user);
     }
-}else{
-    const user = await User.getUnAuthUser(req.params.id);
-    if(!user) res.sendStatus(404);
-    else res.status(200).json(user);
-}
 })
 router.put('/me/promote', checkAuth, limiter, async(req, res) => {
     const prmoteData = Joi.object().keys({
@@ -48,30 +48,29 @@ router.put('/me/promote', checkAuth, limiter, async(req, res) => {
 
         } else {
             const isPromote = await User.promoteToPremium(req.user._id, req.body.cardNumber, req.body.isMonth, req.body.expiresDate);
-            if (isPromote){ 
-                let user=await User.getUserById(req.user._id);
+            if (isPromote) {
+                let user = await User.getUserById(req.user._id);
                 sendmail(user.email, String(user._id), "premium");
                 res.status(200).send({ success: 'check your mail for confirmation ' });
-            }
-            else res.status(400).send({ error: 'can not promote' })
+            } else res.status(400).send({ error: 'can not promote' })
         }
     });
 });
 router.post('/premium/confirm', limiter, async(req, res) => {
-    
-        if (!req.query.id || req.query.id == "") { return res.status(400).send("user id is not given"); }
-        let user = await User.getUserById(req.query.id);
-    
-        let checkConfirm = await User.confirmPremium(user);
-        if (checkConfirm) {
-            user.product = 'premium';
-            await user.save();
-            return res.status(200).send("user is confirmed");
-        } else {
-            return res.status(403).send("user is not confirmed");
-        }
-     
-  
+
+    if (!req.query.id || req.query.id == "") { return res.status(400).send("user id is not given"); }
+    let user = await User.getUserById(req.query.id);
+
+    let checkConfirm = await User.confirmPremium(user);
+    if (checkConfirm) {
+        user.product = 'premium';
+        await user.save();
+        return res.status(200).send("user is confirmed");
+    } else {
+        return res.status(403).send("user is not confirmed");
+    }
+
+
 
 });
 router.put('/me/free', checkAuth, limiter, async(req, res) => {
@@ -84,21 +83,28 @@ router.put('/me/free', checkAuth, limiter, async(req, res) => {
 //GET USER'S PRIVATE PROFILE WITH PLAYER with player info
 router.get('/me-player', checkAuth, limiter, async(req, res) => {
     const userId = req.user._id; // get it from desierialize auth 
-    await spotifySchema.user.find({ _id: userId }, {
-        displayName: 1,
-        email: 1,
-        birthDate: 1,
-        country: 1,
-        product: 1,
-        gender: 1,
-        images: 1,
-        player: 1,
-        userType: 1
-    }).exec().then(user => {
-        if (user) {
-            return res.status(200).send(user);
-        }
-    });
+    const user = await spotifySchema.user.find({ _id: userId });
+    let userOut = [{
+        displayName: user[0].displayName,
+        email: user[0].email,
+        birthDate: user[0].birthDate,
+        country: user[0].country,
+        product: user[0].product,
+        gender: user[0].gender,
+        images: user[0].images,
+        userType: user[0].userType
+    }];
+    userOut[0]['player'] = {};
+    userOut[0].player['source_name'] = user[0].player['sourceName'];
+    userOut[0].player['is_shuffled'] = user[0].player['isShuffled'];
+    userOut[0].player['volume'] = user[0].player['volume'];
+    userOut[0].player['is_repeat'] = user[0].player['isRepeat'];
+    userOut[0].player['current_source'] = user[0].player['currentSource'];
+    userOut[0].player['isPlaylist'] = user[0].player['isPlaylist'];
+    userOut[0].player['currentTimeStampe'] = user[0].player['currentTimeStampe'];
+    userOut[0].player['isRepeatTrack'] = user[0].player['isRepeatTrack'];
+    userOut[0].player['haveQueue'] = user[0].player['currentTrack'] && user[0].player['currentTrack'].trackId ? true : false;
+    return res.status(200).send(userOut);
 
 })
 
@@ -123,8 +129,7 @@ router.put('/me/update', checkAuth, limiter, (req, res) => {
             res.status(400).json({
                 error: err
             })
-        } 
-        else {
+        } else {
             const userId = req.user._id;
             const user = await User.update(req.body.user.repeatedPassword, userId, req.body.user.gender, req.body.user.birthday, req.body.user.displayName, req.body.user.password, req.body.user.email, req.body.user.country, req.body.expiresDate, req.body.cardNumber, req.body.isMonth, req.body.user.newpassword);
             if (user) {

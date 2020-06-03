7bcf21e0fbdba1878e9d4b7cefcbd3f5b0d59bfa
ayaabohadima <ayasabohadima@gmail.com>
2020-06-03 10:24:03 +0200

    status code player

--- routes/browse-routes.js
+++ routes/browse-routes.js
@@ -15,17 +15,16 @@ router.get('/browse/categories/:category_id', limiter, async(req, res) => {
     else res.send({ "category": category });
 })
 router.get('/browse/categories/:category_id/playlists', limiter, async(req, res) => {
-        const categoryId = req.params.category_id;
-        const playlists = await Browse.getCategoryPlaylists(categoryId, req.body.limit, req.body.offset);
-        if (!playlists) res.sendStatus(404); //not found
-        else res.send({ "playlists": playlists });
-    })
-    // get categories
-router.get('/browse/categories', limiter, async(req, res) => {
+    const categoryId = req.params.category_id;
+    const playlists = await Browse.getCategoryPlaylists(categoryId, req.body.limit, req.body.offset);
+    if (!playlists) res.sendStatus(404); //not found
+    else res.send({ "playlists": playlists });
+})
 
+// get categories
+router.get('/browse/categories', limiter, async(req, res) => {
     const category = await Browse.getCategoryies();
     if (category.length == 0) res.sendStatus(404); //not found
     else res.send({ "category": category });
-
 })
 module.exports = router;
--- routes/player-routes.js
+++ routes/player-routes.js
@@ -47,112 +47,104 @@ router.get('/me/player/currently-playing', checkAuth, limiter, async(req, res) =
 
 //get next track playing
 router.get('/me/player/next-playing', checkAuth, limiter, async(req, res) => {
-    const user = await User.getUserById(req.user._id);
-    if (user) {
+        const user = await User.getUserById(req.user._id);
+        if (!user) return res.status(403).send('user is not correct');
         const player = user.player;
-        if (player && player.nextTrack) {
-            var nextPlayingTrack = await Track.getFullTrack(player.nextTrack.trackId, user);
-            if (nextPlayingTrack) {
-                nextPlayingTrack['isPlaylist'] = player.nextTrack.isPlaylist;
-                nextPlayingTrack['playlistId'] = player.nextTrack.playlistId;
-                nextPlayingTrack['isPlayable'] = false;
-                res.send(nextPlayingTrack);
-            } else res.status(404).json({ error: 'track not found' });
-        } else res.status(400).json({ error: 'there is no next  track .' });
-    } else res.status(403).send('user is not correct');
-
-})
-
-//get prev track playing
+        if (!player || !player.nextTrack) return res.status(400).json({ error: 'there is no next  track .' });
+        var nextPlayingTrack = await Track.getFullTrack(player.nextTrack.trackId, user);
+        if (!nextPlayingTrack) return res.status(404).json({ error: 'track not found' });
+        nextPlayingTrack['isPlaylist'] = player.nextTrack.isPlaylist;
+        nextPlayingTrack['playlistId'] = player.nextTrack.playlistId;
+        nextPlayingTrack['isPlayable'] = false;
+        res.send(nextPlayingTrack);
+    })
+    //get prev track playing
 router.get('/me/player/prev-playing', checkAuth, limiter, async(req, res) => {
         const user = await User.getUserById(req.user._id);
-        if (user) {
-            const player = user.player;
-            if (player && player.prevTrack) {
-                var prevPlayingTrack = await Track.getFullTrack(player.prevTrack.trackId, user);
-                if (prevPlayingTrack) {
-                    prevPlayingTrack['isPlaylist'] = player.prevTrack.isPlaylist;
-                    prevPlayingTrack['playlistId'] = player.prevTrack.playlistId;
-                    prevPlayingTrack['pisPlayable'] = false;
-                    res.send(prevPlayingTrack);
-                } else res.status(404).json({ error: 'track not found' });
-            } else res.status(400).json({ error: 'there is no previous track .' });
-        } else res.status(403).send('user is not correct');
+        if (!user) return res.status(403).send('user is not correct');
+        const player = user.player;
+        if (!player || !player.prevTrack) return res.status(400).json({ error: 'there is no previous track .' });
+        var prevPlayingTrack = await Track.getFullTrack(player.prevTrack.trackId, user);
+        if (!prevPlayingTrack) return res.status(404).json({ error: 'track not found' });
+        prevPlayingTrack['isPlaylist'] = player.prevTrack.isPlaylist;
+        prevPlayingTrack['playlistId'] = player.prevTrack.playlistId;
+        prevPlayingTrack['pisPlayable'] = false;
+        res.send(prevPlayingTrack);
     })
     // create queue fo player
 router.post('/createQueue/:sourceId/:trackId', checkAuth, limiter, async(req, res) => {
     if (!req.body.tracksIds || !req.body.sourceType) {
-        if (CheckId([req.params.sourceId, req.params.trackId])) {
-            if (StateValidation(req.query.isPlaylist)) {
-                const sourceId = req.params.sourceId;
-                const trackId = req.params.trackId;
-                const isPlaylist = req.query.isPlaylist;
-                const userId = req.user._id;
-                const createQueue = await User.updateUserPlayer(userId, isPlaylist, sourceId, trackId, undefined, undefined)
-                if (createQueue) res.send(' Queue is created successfully');
-                else res.status(400).send('can not create queue');
-            } else res.status(400).send('isPlaylist is required');
-        } else res.status(403).send('Enter correct ids ');
+        if (!CheckId([req.params.sourceId, req.params.trackId])) return res.status(400).send('Enter correct ids ');
+        if (!StateValidation(req.query.isPlaylist)) return res.status(400).send('isPlaylist is required');
+        const sourceId = req.params.sourceId;
+        const trackId = req.params.trackId;
+        const isPlaylist = req.query.isPlaylist;
+        const userId = req.user._id;
+        const createQueue = await User.updateUserPlayer(userId, isPlaylist, sourceId, trackId, undefined, undefined)
+        if (createQueue) res.send(' Queue is created successfully');
+        else res.status(403).send('can not create queue');
     } else {
         if (!CheckId(req.body.tracksIds) || !CheckId([req.params.trackId]) || !req.body.sourceType) return res.status(403).send('Enter correct ids ');
         const userID = req.user._id;
         const createQueue = await User.updateUserPlayer(userID, undefined, undefined, req.params.trackId, req.body.tracksIds, req.body.sourceType)
         if (createQueue) res.send(' Queue is created successfully');
-        else res.status(400).send('can not create queue');
+        else res.status(403).send('can not create queue');
     }
 })
 router.post('/player/add-to-queue/:playlistId/:trackId', checkAuth, limiter, async(req, res) => {
-        if (CheckId([req.params.playlistId, req.params.trackId])) {
-            if (StateValidation(req.query.isPlaylist)) {
-                const trackId = req.params.trackId;
-                const addToQueue = await User.addToQueue(req.user._id, trackId, req.query.isPlaylist, req.params.playlistId);
-                if (addToQueue == 0) res.status(400).send('can not add queue ! ');
-                else res.send('add successfully');
-            } else res.status(400).send('isPlaylist is required');
-        } else res.status(403).send('Enter correct ids');
-    })
-    // skip to next track 
+    if (!CheckId([req.params.playlistId, req.params.trackId])) return res.status(400).send('Enter correct ids');
+    if (!StateValidation(req.query.isPlaylist)) return res.status(400).send('isPlaylist is required');
+    const trackId = req.params.trackId;
+    const addToQueue = await User.addToQueue(req.user._id, trackId, req.query.isPlaylist, req.params.playlistId);
+    if (addToQueue == 0) res.status(403).send('can not add queue ! ');
+    else res.send('add successfully');
+})
+
+// skip to next track 
 router.post('/me/player/next-playing', checkAuth, nextLimiter, async(req, res) => {
-        // allow for 6 requests per hour only
-        if (req.user.product == "free") {
-            if (req.rateLimit.current > 6) {
-                res.status(429).json({ "error": "rate limit reached for free user skip tracks" });
-                return 0;
-            }
+    // allow for 6 requests per hour only
+    if (req.user.product == "free") {
+        if (req.rateLimit.current > 6) {
+            res.status(429).json({ "error": "rate limit reached for free user skip tracks" });
+            return 0;
         }
-        const user = await User.getUserById(req.user._id);
-        if (!user) return res.status(403).send('user is not correct');
-        const skip = await Player.skipNext(user)
-        if (!skip) res.status(404).json({ error: 'track not found' })
-        else return res.send(skip);
-    })
-    // skip to prev track
+    }
+    const user = await User.getUserById(req.user._id);
+    if (!user) return res.status(403).send('user is not correct');
+    const skip = await Player.skipNext(user)
+    if (!skip) res.status(404).json({ error: 'track not found' })
+    else return res.send(skip);
+})
+
+// skip to prev track
 router.post('/me/player/prev-playing', checkAuth, nextLimiter, async(req, res) => {
-        if (req.user.product == "free") {
-            if (req.rateLimit.current > 6) {
-                res.status(429).json({ "error": "rate limit reached for free user skip tracks" });
-                return 0;
-            }
+    if (req.user.product == "free") {
+        if (req.rateLimit.current > 6) {
+            res.status(429).json({ "error": "rate limit reached for free user skip tracks" });
+            return 0;
         }
-        const user = await User.getUserById(req.user._id);
-        if (!user) return res.status(403).send('user is not correct');
-        const skip = await Player.skipPrevious(user);
-        if (skip) return res.send(skip);
-        else res.status(404).json({ error: 'track not found' })
-    })
-    // get user queue 
+    }
+    const user = await User.getUserById(req.user._id);
+    if (!user) return res.status(403).send('user is not correct');
+    const skip = await Player.skipPrevious(user);
+    if (skip) return res.send(skip);
+    else res.status(404).json({ error: 'track not found' })
+})
+
+// get user queue 
 router.get('/me/queue', checkAuth, limiter, async(req, res) => {
-        const userId = req.user._id;
-        const tracks = await User.getQueue(userId);
-        if (!tracks) res.status(400).json({ error: 'couldnt get queue' });
-        else res.status(200).json(tracks);
-    })
-    //to repeat
+    const userId = req.user._id;
+    const tracks = await User.getQueue(userId);
+    if (!tracks) res.status(404).json({ error: 'NOT FOUND QUEUE' });
+    else res.status(200).json(tracks);
+})
+
+//to repeat
 router.put('/player/repeat', checkAuth, limiter, async(req, res) => {
     const userId = req.user._id;
     if (StateValidation(req.query.state)) {
         const repeat = User.repreatPlaylist(userId, req.query.state);
-        if (!repeat) res.status(400).json({ error: 'could not repeat playlist' });
+        if (!repeat) res.status(403).json({ error: 'could not repeat playlist' });
         else res.status(200).json({ success: 'is_repeat playlist: ' + req.query.state })
     } else req.status(400).send('state is required');
 })
@@ -161,14 +153,14 @@ router.put('/player/repeat', checkAuth, limiter, async(req, res) => {
 router.put('/me/player/play', checkAuth, limiter, async(req, res) => {
         const userId = req.user._id;
         const player = User.resumePlaying(userId);
-        if (!player) res.status(404).json({ error: 'could not resume playing' });
+        if (!player) res.status(403).json({ error: 'could not resume playing' });
         else res.status(204).json({ success: 'resumed playing' })
     })
     // pause player 
 router.put('/me/player/pause', checkAuth, limiter, async(req, res) => {
         const userId = req.user._id;
         const player = User.pausePlaying(userId);
-        if (!player) res.status(404).json({ error: 'could not resume playing' });
+        if (!player) res.status(403).json({ error: 'could not resume playing' });
         else res.status(204).json({ success: 'paused playing' })
     })
     //toggle shuffle
@@ -177,29 +169,27 @@ router.put('/me/player/shuffle', checkAuth, limiter, async(req, res) => {
             const userId = req.user._id;
             const state = req.query.state;
             const isShuffle = await User.setShuffle(state, userId);
-            if (!isShuffle) res.status(400).json({ error: 'can not shuffle ' });
+            if (!isShuffle) res.status(403).json({ error: 'can not shuffle ' });
             else res.status(200).json({ success: 'Do successfully' });
         } else res.status(400).send('state is required');
     })
     // get recent played
 router.get('/me/player/recently-played', checkAuth, limiter, async(req, res) => {
         const user = await User.getUserById(req.user._id);
-        if (user) {
-            const playHistory = await Player.getRecentTracks(user, req.query.limit);
-            if (!playHistory) res.status(400).json({ error: 'can not get playhistory' });
-            else res.status(200).json(playHistory);
-        } else res.status(403).send('this user is deleted');
+        if (!user) return res.status(403).send('this user is deleted');
+        const playHistory = await Player.getRecentTracks(user, req.query.limit);
+        if (!playHistory) res.status(404).json({ error: 'can not found playhistory' });
+        else res.status(200).json(playHistory);
+
     })
     // add to recent played
 router.put('/me/player/recently-played/:source_id/:track_id', checkAuth, limiter, async(req, res) => {
-    if (CheckId([req.params.track_id, req.params.source_id])) {
-        const user = await User.getUserById(req.user._id);
-        if (user) {
-            const playHistory = await Player.addRecentTrack(user, req.params.track_id, req.query.sourceType, req.params.source_id);
-            if (!playHistory) res.status(400).json({ error: 'can not add to playhistory ' });
-            else res.status(203).json({ 'success': 'added successfully' });
-        } else res.status(403).send('user is not correct')
-    } else res.status(403).send('Enter correct ids');
+    if (!CheckId([req.params.track_id, req.params.source_id])) return res.status(400).send('Enter correct ids');
+    const user = await User.getUserById(req.user._id);
+    if (!user) return res.status(403).send('user is not correct');
+    const playHistory = await Player.addRecentTrack(user, req.params.track_id, req.query.sourceType, req.params.source_id);
+    if (!playHistory) res.status(403).json({ error: 'can not add to playhistory ' });
+    else res.status(203).json({ 'success': 'added successfully' });
 })
 
 module.exports = router;

074c93d7d0c4556e2d65bc2919b1a32b65e48e33
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-06-10 10:04:49 +0200

    challenges

--- seed.js
+++ seed.js
@@ -1,7 +1,8 @@
 const localhost = 'mongodb://localhost:27017/test';
 const mlab = "bahaa:123456b@ds157834.mlab.com:57834/spotify-demo";
+//String(process.env.CONNECTION_STRING)
 module.exports = {
-    "undefined": String(process.env.CONNECTION_STRING),
+    "undefined": localhost,
     "dev": "localhost/DEV_DB_NAME",
     "prod": "localhost/PROD_DB_NAME"
 }

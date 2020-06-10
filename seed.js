ad683343873a5539beb3b381585b1fad04e2f7f0
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-06-10 08:53:33 +0200

    task division updated

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
-const localhost = 'localhost:27017/coco';
+const localhost = 'mongodb://localhost:27017/test';
 const mlab = "bahaa:123456b@ds157834.mlab.com:57834/spotify-demo";
 module.exports = {
-    "undefined": localhost,
+    "undefined": String(process.env.CONNECTION_STRING),
     "dev": "localhost/DEV_DB_NAME",
     "prod": "localhost/PROD_DB_NAME"
 }
